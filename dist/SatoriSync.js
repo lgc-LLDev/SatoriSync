e1513a55c11837e2351ba88f75c6eede1415dba5
student_2333 <lgc2333@126.com>
2024-03-09 15:20:38 +0800

    x

---
+++ dist/SatoriSync.js
@@ -0,0 +1,29 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/index.ts
+var src_exports = {};
+__export(src_exports, {
+  apply: () => apply
+});
+module.exports = __toCommonJS(src_exports);
+function apply(ctx) {
+  const logger = ctx.logger("sync");
+  logger.info("\u64CD\u4F60\u5988\uFF0C\u4E0D\u505A\u4E86\uFF0C\u{1F62D}\u{1F62D}\u{1F62D}");
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/// <reference types="levi-satori" />
+
+export function apply(ctx: LeviSatori.Context) {
+  const logger = ctx.logger('sync');
+  logger.info('操你妈，不做了，😭😭😭');
+}
